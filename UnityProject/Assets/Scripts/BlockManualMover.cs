c0ca3f09c83c434f37997124ba2d78bad5a5200a
Andrew Carvalho <aj.carvalho@andrewcarvalho.com>
2015-02-08 14:29:24 -0500

    manual block mover scene with placed blocks

---
+++ UnityProject/Assets/Scripts/BlockManualMover.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockManualMover : MonoBehaviour
+{
+    public float m_runSpeed = 300;
+
+    public void Start()
+    {
+        Rigidbody2D body = GetComponent<Rigidbody2D>();
+        body.velocity = new Vector2( -m_runSpeed, 0 );
+    }
+}
