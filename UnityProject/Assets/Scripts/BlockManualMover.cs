27c0bbd3f9c96d7a508116e9af5917d3d1de6390
Andrew Carvalho <aj.carvalho@andrewcarvalho.com>
2015-02-08 17:08:54 -0500

    iPad?

--- UnityProject/Assets/Scripts/BlockManualMover.cs
+++ UnityProject/Assets/Scripts/BlockManualMover.cs
@@ -6,14 +6,11 @@ public class BlockManualMover : MonoBehaviour
     public float m_runStartSpeed = 5;
     public float m_speedIncrease = 1;
 
-    private float m_currentSpeed;
+    private float m_currentSpeed = 0;
     private Rigidbody2D m_body;
 
     public void Start()
     {
-        m_currentSpeed = m_runStartSpeed;
-        //m_body = GetComponent<Rigidbody2D>();
-        //m_body.velocity = new Vector2( -m_currentSpeed, 0 );
     }
 
     public void Update()
@@ -21,5 +18,10 @@ public class BlockManualMover : MonoBehaviour
         m_currentSpeed += Time.deltaTime * m_speedIncrease;
         //m_body.velocity = new Vector2( -m_currentSpeed, 0 );
         this.gameObject.transform.Translate( Vector3.left * m_currentSpeed );
+    }
+
+    public void StartRunning()
+    {
+        m_currentSpeed = m_runStartSpeed;
     }
 }
