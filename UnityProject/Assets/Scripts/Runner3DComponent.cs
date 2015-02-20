93656276934331b0821e9ddc38ab47bbb8a03c75
Andrew Carvalho <aj.carvalho@andrewcarvalho.com>
2015-02-20 13:29:02 -0500

    the things, not all of them though

--- UnityProject/Assets/Scripts/Runner3DComponent.cs
+++ UnityProject/Assets/Scripts/Runner3DComponent.cs
@@ -8,9 +8,16 @@ public class Runner3DComponent : MonoBehaviour
 
     public float m_targetNormalizedHorizontalScreenPosition = 0.2f;
     public float m_returnAccel = 1;
+    public float m_extraGravity = 1;
+
+    private float m_previousForce = 0;
 
     private Rigidbody m_body;
     private Vector2 m_screenSize;
+
+    //private ConfigurableJoint m_joint;
+
+    //public float m_targetPosition = 0f;
 
     // Use this for initialization
     void Start()
@@ -18,12 +25,17 @@ public class Runner3DComponent : MonoBehaviour
         m_body = GetComponent<Rigidbody>();
         Camera camera = GameObject.Find( "Main Camera" ).GetComponent<Camera>();
         m_screenSize = new Vector2( camera.orthographicSize * camera.aspect, camera.orthographicSize ) * 2;
+
+        //m_joint = GetComponentInParent<ConfigurableJoint>();
+        //m_joint.xDrive = new JointDrive();
+        //m_joint.targetPosition = new Vector3( m_targetPosition, this.transform.position.y, this.transform.position.z );
     }
 
     // Update is called once per frame
     void Update()
     {
         // always try to run to a specific X value
+        //m_joint.targetPosition = new Vector3( m_targetPosition, this.transform.position.y, this.transform.position.z );
 
         // accel is 0 is on the point, positive if behind and negative if in front
         if ( this.transform.position.x != m_targetNormalizedHorizontalScreenPosition * m_screenSize.x - m_screenSize.x / 2 )
@@ -37,8 +49,18 @@ public class Runner3DComponent : MonoBehaviour
             {
                 distanceOut = Mathf.Clamp( distanceOut, -2, -1 );
             }
+            float force = distanceOut * m_returnAccel;
             m_body.AddForce( Vector3.left * distanceOut * m_returnAccel, ForceMode.Force );
+            if ( Mathf.Sign(force) != Mathf.Sign(m_previousForce) )
+            {
+                // set velocity to zero
+                m_body.velocity = new Vector3( 0, m_body.velocity.y, 0 );
+            }
+            m_previousForce = force;
+            m_body.AddForce( Vector3.up * -m_extraGravity, ForceMode.Force );
             //m_body.velocity = -Vector2.right * distanceOut * m_returnAccel;
+
+
         }
 
 
