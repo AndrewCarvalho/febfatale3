1d7889bb47db590cddbee9524f475ba39b316fdc
Andrew Carvalho <aj.carvalho@andrewcarvalho.com>
2015-02-20 15:35:18 -0500

    Fixed splash screen

---
+++ UnityProject/Assets/PauseTime.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseTime : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+    {
+        Time.timeScale = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    public void StartTime()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void StopTime()
+    {
+        Time.timeScale = 0;
+    }
+}
